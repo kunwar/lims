62b3c3fbb34354054be3c5b949fcaaf4fede36b4
Kunwar Singh <dev@kunwar.me>
2023-03-12 13:54:55 +0530

    Update settings.py

--- lims/settings.py
+++ lims/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = "/var/www/starlab.com/static/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

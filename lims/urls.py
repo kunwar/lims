4b4146a1ec572f1ef1370ba2c8347e94583f1a32
Kunwar Singh <dev@kunwar.me>
2023-03-13 17:48:12 +0530

    Update urls.py

--- lims/urls.py
+++ lims/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('lab/', include('lab.urls')),
+    path('', include('lab.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
